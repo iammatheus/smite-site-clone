479deaa3756dad2648da7dfb4e48e08259ddcdfc
Matheus Ferreira <matheus.ferreira9@hotmail.com>
2021-05-08 01:57:33 -0300

    Interaction and animation 80% complete

--- scrips.js
+++ scrips.js
@@ -1,7 +1,8 @@
 let more_icon = document.querySelector('.material-icons')
 let more = document.querySelector('.more')
 let dropdown = document.querySelector('.dropdown')
-
+let smallGodContainerScale = document.querySelectorAll('.small-god-container-scale')
+let smallButtonContainer = document.querySelector('.small-button-container')
 
 more.addEventListener('mouseover', () => {
    dropdown.style.display = 'flex'
@@ -23,3 +24,23 @@ dropdown.addEventListener('mouseout', () => {
    dropdown.style.opacity = 0
    more_icon.style.transform = 'rotate(180deg)'
 })
+
+
+
+
+//Small Gods - Adding scale to the image when moving and removing the scale when clicking
+for(let i = 0; i < smallGodContainerScale.length; i++){
+   smallGodContainerScale[i].onclick = function(){
+      let el = smallGodContainerScale[0];
+      while(el){
+         if(el.tagName === 'DIV'){
+            el.classList.remove('small-god-border');
+            el.classList.add('small-god-container-scale');
+         }
+         el = el.nextSibling;
+      }      
+      this.classList.add('small-god-border');
+      this.classList.remove('small-god-container-scale');
+      smallButtonContainer.classList.remove('small-god-container-scale')
+   };
+}
